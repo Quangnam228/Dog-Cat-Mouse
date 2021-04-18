11c5de0a3ef0868da27482bbcfd174075858257f
QuangNam <quangnam228278@gmail.com>
2021-04-18 17:29:35 +0700

    sayHi method for Dog

--- learngit/Dog.js
+++ learngit/Dog.js
@@ -1,7 +1,15 @@
-function Dog(){
+var chalk = requires('chalk');
+function Dog(name) {
     this.stomach =[];
+    this.name = name
 }
 
 Dog.prototype.eat = function(Cat){
     this.stomach.push(cat);
 };
+
+Dog.prototype.SayHi = function(){
+    console.log("Hi! I am a dog,My name is" +chalk.blue(this.name));
+}
+
+module.exports = Dog;
